21f94058334b14c958c36986c463cc9da5ce24f0
pykroth <pyrakroth@gmail.com>
2024-12-31 12:58:41 -0600

    2 new images

--- src/Components/GalleryComponent.jsx
+++ src/Components/GalleryComponent.jsx
@@ -51,10 +51,11 @@ import g48 from '../Images/g48.jpg';
 import g50 from '../Images/g50.jpg';
 import g51 from '../Images/g51.jpg';
 import g52 from '../Images/g52.jpg';
-
+import g53 from '../Images/g53.jpg';
+import g54 from '../Images/g54.jpg';
 
 const Gallery = () => {
-  const images = [g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g19, g20, g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37, g38, g39, g40, g41,g42,g43,g44,g45,g46,g47,g48,g50,g51,g52];
+  const images = [g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g19, g20, g21, g22, g23, g24, g25, g26, g27, g28, g29, g30, g31, g32, g33, g34, g35, g36, g37, g38, g39, g40, g41,g42,g43,g44,g45,g46,g47,g48,g50,g51,g52,g53,g54];
   const [visibleImages, setVisibleImages] = useState(new Array(images.length).fill(false)); // Set all images as initially invisible
 
   const imageRefs = useRef(images.map(() => React.createRef()));
